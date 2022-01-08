e352fb2e3fd8ee89b1385f36c1266f6fa9152f5f
Clares <2984502149@qq.com>
2022-01-08 11:14:28 +0800

    a test

--- TEMPLATE_PROJECT/CODES/DRIVER/STEP_MOTOR.c
+++ TEMPLATE_PROJECT/CODES/DRIVER/STEP_MOTOR.c
@@ -3,14 +3,14 @@
  * @Author       : FZU Liao
  * @Date         : 2022-01-07 23:02:40
  * @LastEditors  : Liao
- * @LastEditTime : 2022-01-08 11:00:43
+ * @LastEditTime : 2022-01-08 11:14:16
  * @FilePath     : \TEMPLATE_PROJECT\CODES\DRIVER\STEP_MOTOR.c
  * Copyright 2022 FZU Liao, All Rights Reserved. 
  */
 
 #include "STEP_MOTOR.h"
 
-void STEP_MOTOR_INIT(){
+void STEP_MOTOR_INIT() {
     PWM_INIT_PWMCHANNEL(STEP_PWM_OUTPUT_PIN,12500,0);
     GPIO_SET_MODE(GPIO_P6,Pin_0|Pin_4,GPIO_OUT_PULL_PUSH_MODE);
     STEP_MOTOR_MODE(FORWARD);
