942a671dfceba7e01bd17785135c44707fe018a9
Clares <2984502149@qq.com>
2022-01-10 23:11:11 +0800

    a lcd test

--- TEMPLATE_PROJECT/CODES/USERS/main.c
+++ TEMPLATE_PROJECT/CODES/USERS/main.c
@@ -8,14 +8,14 @@ void SYS_INIT_ALL(){
     EXINT_ADDRESS_INIT();
     TIMER_ADDRESS_INIT();
     IQR_Enable_ALL();
-    TASK_INIT_TASKSYS();  //多任务系统,开启则占用定时器TIMER0
+    //TASK_INIT_TASKSYS();  //多任务系统,开启则占用定时器TIMER0
 }
 
 void main(){
     SYS_INIT_ALL();
     lcd_init();
     while(1){
-        lcd_clear(WHITE);
+        lcd_clear(BLUE);
         lcd_showchar(1,1,'a');  
     }
 }
