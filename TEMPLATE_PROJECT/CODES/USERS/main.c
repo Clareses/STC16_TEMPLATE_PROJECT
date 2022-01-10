ab4ed34462b9c1cefcb2e15594e548437d3c9a0d
Clares <2984502149@qq.com>
2022-01-10 22:07:33 +0800

    添加了zf库

--- TEMPLATE_PROJECT/CODES/USERS/main.c
+++ TEMPLATE_PROJECT/CODES/USERS/main.c
@@ -2,6 +2,7 @@
 #include "MyControlSys.h"
 
 void SYS_INIT_ALL(){
+    BOARD_INIT();
 	DELAY_INIT();
     EXINT_ADDRESS_INIT();
     TIMER_ADDRESS_INIT();
@@ -9,18 +10,7 @@ void SYS_INIT_ALL(){
     TASK_INIT_TASKSYS();  //多任务系统,开启则占用定时器TIMER0
 }
 
-//UserCode-------------------------------------------------------------------------------------------
-
-
-
-//UserCode-------------------------------------------------------------------------------------------
-
 void main(){
     SYS_INIT_ALL();
-    MySYS_INIT();
-    //UserCode---------------------------------------------------------------------------------------
-	TASK My_CAR_Routine;
-    TASK_INIT_NEWTASK(&My_CAR_Routine,1,My_routineTask);
-    TASK_ADD_TASK(&My_CAR_Routine);
-    //UserCode---------------------------------------------------------------------------------------
+
 }
