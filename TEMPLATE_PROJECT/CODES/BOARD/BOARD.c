8393db544eadea30c013b5cb6e89d872db4f38af
Clares <2984502149@qq.com>
2022-01-10 21:38:10 +0800

    修改了Board_INIT

--- TEMPLATE_PROJECT/CODES/BOARD/BOARD.c
+++ TEMPLATE_PROJECT/CODES/BOARD/BOARD.c
@@ -3,15 +3,30 @@
  * @Author       : FZU Liao
  * @Date         : 2021-12-28 21:57:55
  * @LastEditors  : Liao
- * @LastEditTime : 2021-12-28 22:24:30
- * @FilePath     : \Board\board.c
+ * @LastEditTime : 2022-01-10 21:37:51
+ * @FilePath     : \TEMPLATE_PROJECT\CODES\BOARD\BOARD.c
  * Copyright 2021 FZU Liao, All Rights Reserved. 
  */
 
 #include "BOARD.h"
 
 void BOARD_INIT(){
-
+    P0M0 = 0x00;
+	P0M1 = 0x00;
+	P1M0 = 0x00;
+	P1M1 = 0x00;
+	P2M0 = 0x00;
+	P2M1 = 0x00;
+	P3M0 = 0x00;
+	P3M1 = 0x00;
+	P4M0 = 0x00;
+	P4M1 = 0x00;
+	P5M0 = 0x00;
+	P5M1 = 0x00;
+	P6M0 = 0x00;
+	P6M1 = 0x00;
+	P7M0 = 0x00;
+	P7M1 = 0x00;
 }
 
 void IQR_Disable_ALL(){
