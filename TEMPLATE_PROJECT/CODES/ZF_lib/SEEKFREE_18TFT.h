020e702a93b02af4b1147265bb78acf94a9495e3
Clares <2984502149@qq.com>
2022-01-10 22:09:46 +0800

    bugFix

--- TEMPLATE_PROJECT/CODES/ZF_lib/SEEKFREE_18TFT.h
+++ TEMPLATE_PROJECT/CODES/ZF_lib/SEEKFREE_18TFT.h
@@ -81,16 +81,16 @@
 
 
 //-------常用颜色在SEEKFREE_FONT.h文件中定义----------
-//#define RED          	    0xF800	//红色
-//#define BLUE         	    0x001F  //蓝色
-//#define YELLOW       	    0xFFE0	//黄色
-//#define GREEN        	    0x07E0	//绿色
-//#define WHITE        	    0xFFFF	//白色
-//#define BLACK        	    0x0000	//黑色 
-//#define GRAY  			0X8430 	//灰色
-//#define BROWN 			0XBC40 	//棕色
-//#define PURPLE    		0XF81F	//紫色
-//#define PINK    		    0XFE19	//粉色
+#define RED          	    0xF800	//红色
+#define BLUE         	    0x001F  //蓝色
+#define YELLOW       	    0xFFE0	//黄色
+#define GREEN        	    0x07E0	//绿色
+#define WHITE        	    0xFFFF	//白色
+#define BLACK        	    0x0000	//黑色 
+#define GRAY  			0X8430 	//灰色
+#define BROWN 			0XBC40 	//棕色
+#define PURPLE    		0XF81F	//紫色
+#define PINK    		    0XFE19	//粉色
 
 
 
